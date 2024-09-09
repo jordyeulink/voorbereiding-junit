ebc6c287f732e2c7c5d7582fb3f97acc1f068a3c
jordy Eulink <jordy.eulink31@gmail.com>
2024-09-09 22:12:31 +0200

    les 2 week 2 voorbereiding

--- src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
@@ -3,6 +3,13 @@ package nl.han.ica.oose.dea.testedfizzbuzz;
 public class FizzBuzzExecutor {
 
     public String execute(int i){
-        return null;
+        if(i%15 == 0) {
+            return "fizzbuzz";
+        } else if(i%5 == 0) {
+            return "buzz";
+        } else if(i%3 == 0) {
+            return "fizz";
+        }
+        return "" + i;
     }
 }
---
+++ src/test/java/FizzBuzzExecutorTester.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import nl.han.ica.oose.dea.testedfizzbuzz.FizzBuzzExecutor;
+import org.junit.jupiter.api.Assertions;
+
+
+public class FizzBuzzExecutorTester {
+
+    private FizzBuzzExecutor sut;
+    @BeforeEach
+    public void setup(){
+        sut = new FizzBuzzExecutor();
+    }
+
+    @Test
+    void executeWithValidIntTest(){
+        var testValue = sut.execute(37);
+        Assertions.assertEquals("37", testValue);
+    }
+
+    @Test
+    void executeWithValidfizzTest(){
+        var testValue = sut.execute(3);
+        Assertions.assertEquals("fizz", testValue);
+    }
+
+    @Test
+    void executeWithValidbuzzTest(){
+        var testValue = sut.execute(5);
+        Assertions.assertEquals("buzz", testValue);
+    }
+
+    @Test
+    void executeWithValidfizzbuzzTest(){
+        var testValue = sut.execute(45);
+        Assertions.assertEquals("fizzbuzz", testValue);
+    }
+}
